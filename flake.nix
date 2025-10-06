1c8155ff42654ea2f440e249b3a7630b64b02c51
Kyle Manna <kyle@kylemanna.com>
2025-10-05 19:04:04 -0700

    flake: Add packages output and expose code-cursor package
    • Added packages output to flake.nix for direct package access
    • Exposed code-cursor package for all supported systems
    • Enables building with 'nix build .#code-cursor' command
    • Improves flake structure and makes packages more discoverable

--- flake.nix
+++ flake.nix
@@ -5,20 +5,34 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       inherit (nixpkgs) lib;
 
       # Aggregate all individual overlays
-      aggregateOverlays = final: prev:
+      aggregateOverlays =
+        final: prev:
         with prev.lib;
         let
           overlayList = map import (import ./overlays.nix);
         in
-          foldl' (flip extends) (_: prev) overlayList final;
+        foldl' (flip extends) (_: prev) overlayList final;
     in
     {
       # Default overlay (aggregates all individual overlays)
       overlays.default = aggregateOverlays;
+
+      # Expose packages for each system
+      packages = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          overlayedPkgs = pkgs.extend aggregateOverlays;
+        in
+        {
+          code-cursor = overlayedPkgs.code-cursor;
+        }
+      );
     };
 }
