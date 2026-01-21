8939df745a5f35f638220b8a75e295db3532fe6c
Kyle Manna <kyle@kylemanna.com>
2026-01-20 20:33:36 -0800

    code-cursor: Update to version 2.4.7
    - Updated version from 2.3.41 to 2.4.7
    
    - Updated URL and hash accordingly

--- packages/code-cursor/package.nix
+++ packages/code-cursor/package.nix
@@ -19,8 +19,8 @@ let
 
   sources = {
     x86_64-linux = fetchurl {
-      url = "https://downloads.cursor.com/production/2ca326e0d1ce10956aea33d54c0e2d8c13c58a32/linux/x64/Cursor-2.3.41-x86_64.AppImage";
-      hash = "sha256-ItUgknMzSDeXxN3Yi/pz2wZoz7vVVqx9nGXuGmbHbXc=";
+      url = "https://downloads.cursor.com/production/ca0f9bf806f235ea014a22712cbcbf5e88ca77e9/linux/x64/Cursor-2.4.7-x86_64.AppImage";
+      hash = "sha256-wfZVFFk3un3NonXSFP+MaCpyGMrvepdjV6TEtP0GYE8=";
     };
   };
 
@@ -30,7 +30,7 @@ in
   inherit useVSCodeRipgrep;
   commandLineArgs = finalCommandLineArgs;
 
-  version = "2.3.41";
+  version = "2.4.7";
   pname = "cursor";
 
   # You can find the current VSCode version in the About dialog:
